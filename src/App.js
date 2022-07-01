216b87d33f7e8f3dcddcb9e298bdfce6f909bd0f
youssef535 <youssefsoltani938@gmail.com>
2022-07-01 17:31:25 +0200

    clean the code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
 import Navigationbar from './components/NavBar';
 import { Routes , Route, Link } from "react-router-dom";
 import NFT from './components/NFT';
 import Exchange from './components/Exchange';
 import News from './components/News';
 import coins from "./pages/Coin";
-import Banner from "./components/Banner/Banner.jsx"
-import Table from "./components/CoinTable"
+import Banner from "./components/Banner/Banner.jsx";
+import Table from "./components/CoinTable";
 
 
 function App() {
@@ -16,14 +16,14 @@ function App() {
         <Navigationbar />
         <Banner/>
         <Table/>
-        <>
-            <Routes>
-              <Route path="/NFT" element={<NFT />} />
-              <Route path="/Exchange" element={<Exchange />} />
-              <Route path="/News" element={<News />} />
-              <Route path="/coins/:id" element={coins} exact />
-            </Routes>
-            </>
+          <>
+              <Routes>
+                <Route path="/NFT" element={<NFT />} />
+                <Route path="/Exchange" element={<Exchange />} />
+                <Route path="/News" element={<News />} />
+                <Route path="/coins/:id" element={coins} exact />
+              </Routes>
+          </>
       </div>
   );
 }
