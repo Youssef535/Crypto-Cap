4e473ab466791bfc2ea17f9a39362b7fad4b8dac
youssef535 <youssefsoltani938@gmail.com>
2022-08-03 22:10:37 +0200

    Update News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,20 +3,9 @@ import axios from 'axios'
 import { Container,Card } from 'react-bootstrap'
 
 
-const options = {
-  method: 'GET',
-  url: 'https://crypto-news-live3.p.rapidapi.com/news',
-  headers: {
-    'X-RapidAPI-Key': '2ab96d9ec9msh53ad8c155826209p15267ejsn9831b1935b52',
-    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
-  }
-};
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+
+
 
 
 const News = () => {
@@ -25,12 +14,10 @@ const News = () => {
   const [search, setSearch] = useState("");
   
   
-
-  if (!news) return ("Loading...");
-
   return (
     <div>
-      <h1>{news.bitcoin}</h1>
+      <h1>News Page </h1>
+      <h4>api</h4>
     </div>  
   )
 }
