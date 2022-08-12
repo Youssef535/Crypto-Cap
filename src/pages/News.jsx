d8d04357fa2c0d6e1bfd2480aabbdd599b117996
youssef535 <youssefsoltani938@gmail.com>
2022-08-12 21:53:09 +0200

    Update News API

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,21 +1,32 @@
-import { useState, useEffect, React } from 'react'
-import axios from 'axios'
-import { Container,Card } from 'react-bootstrap'
+import { useState, useEffect, React } from 'react';
+import { Container,Card } from 'react-bootstrap';
+import axios from 'axios';
 
 
 
+const options = {
+  method: 'GET',
+  url: 'https://crypto-news-live3.p.rapidapi.com/news',
+  headers: {
+    'X-RapidAPI-Key': '2ab96d9ec9msh53ad8c155826209p15267ejsn9831b1935b52',
+    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+  }
+};
 
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 const News = () => {
 
   const [news, setNews] = useState([]);
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(false);
-  
   return (
     <div>
       <h1>News Page </h1>
-      <h4>api</h4>
+      <h4>Fetching api here for the news section</h4>
+      <h4>{news.image}</h4>
     </div>  
   )
 }
