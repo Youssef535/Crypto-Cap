ceedbec56a5ac5bba4e2ceb80082033e1ebd6e3d
youssef535 <youssefsoltani938@gmail.com>
2022-08-15 21:17:45 +0200

    Update News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -27,7 +27,7 @@ const News = () => {
 
   return (
     <div>
-      <h1>Last News  </h1>
+      <h1 className='p-4'>Last News  </h1>
        {/* display all trending news  */}
       <Container>
         <Row>
@@ -35,12 +35,13 @@ const News = () => {
           {news.map(news => (
             <Col key={news.title} md={4}>
               <Card className='bg-dark' style={{ margin:"10px", cursor:"pointer" }}>
-                <Card.Img variant="top" src={news.urlToImage} />
+                <Card.Img variant="top" src={news.urlToImage}  />
                 <Card.Body>
                   <Card.Title>{news.title}</Card.Title>
                   <Card.Text>
                     {/* working with the news character */}
-                    {news.description}
+                    {news.description && news.description.length > 80 ? news.description.substring(0, 80) + "..." : news.description}
+                    
                   </Card.Text>
                   <Button variant="primary" className='link' href={news.url}>Read More</Button>
                 </Card.Body>
