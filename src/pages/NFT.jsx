685408a943818b4a52b3110f43e3704ae9b3eaf8
youssef535 <youssefsoltani938@gmail.com>
2022-09-26 17:51:29 +0200

    Update NFT
    Start working on the NFT page after a While

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 import { Container, Form, Row, Table, Pagination } from 'react-bootstrap';
+import axios from 'axios';
+import { NFTcoin } from '../Config/api';
 
 
 
 
-const NFT = () => {
+
+
+
+export const NFT = () => {
   return (
-    <h1>NFT Market </h1>
+    <h1>Top NFT Collections</h1>
   )
 }
+
+
 
 export default NFT
