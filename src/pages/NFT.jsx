e7c530feca538ab078e11e6a8bc2dddb2b2b05e7
youssef535 <youssefsoltani938@gmail.com>
2022-09-27 19:12:16 +0200

    Update NFT.jsx

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -4,23 +4,38 @@ import axios from 'axios';
 
 
 
-const NFTS = () => {
+const NFT = () => {
 
+    
   
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   
-}
 
-
-
-
-export const NFT = () => {
+  const options = {
+    method: 'GET',
+    url: 'https://top-nft-sales.p.rapidapi.com/collections/1d',
+    headers: {
+      'X-RapidAPI-Key': '2ab96d9ec9msh53ad8c155826209p15267ejsn9831b1935b52',
+      'X-RapidAPI-Host': 'top-nft-sales.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
   return (
-    <h1>Top NFT Collections</h1>
+    <h1>NFT Marketplace</h1>
   )
 }
 
 
 
-export default NFT
+
+
+
+
+
+export default NFT;
