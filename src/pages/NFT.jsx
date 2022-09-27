d470f1c7515c8ceaffb07ad5b0aa0bf1f3a95ad1
youssef535 <youssefsoltani938@gmail.com>
2022-09-27 21:31:17 +0200

    Update NFT.jsx

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -1,33 +1,39 @@
 import {React,useState,useEffect} from 'react'
-import { Container, Form, Row, Table, Pagination } from 'react-bootstrap';
-import axios from 'axios';
+import { Container, Form, Row, Table, Pagination } from 'react-bootstrap'
+import axios from 'axios'
 
 
+
+const options = {
+  method: 'GET',
+  url: 'https://top-nft-collections-and-sales.p.rapidapi.com/sales/1d',
+  headers: {
+    'X-RapidAPI-Key': '2ab96d9ec9msh53ad8c155826209p15267ejsn9831b1935b52',
+    'X-RapidAPI-Host': 'top-nft-collections-and-sales.p.rapidapi.com'
+  }
+};
 
 const NFT = () => {
 
     
-  
+  const [NftNews, SetnftNews ] = useState();
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   
 
-  const options = {
-    method: 'GET',
-    url: 'https://top-nft-sales.p.rapidapi.com/collections/1d',
-    headers: {
-      'X-RapidAPI-Key': '2ab96d9ec9msh53ad8c155826209p15267ejsn9831b1935b52',
-      'X-RapidAPI-Host': 'top-nft-sales.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  useEffect(() => {
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  },[]);
+
   return (
-    <h1>NFT Marketplace</h1>
+    
+    <h1>NFT Market Place</h1>
+    
   )
 }
 
