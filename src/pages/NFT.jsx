bf5fa89d4b9a9b92a5ca7363f106eae32a1d8feb
youssef535 <youssefsoltani938@gmail.com>
2022-09-27 16:28:40 +0200

    Update NFT.jsx

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -6,13 +6,10 @@ import axios from 'axios';
 
 const NFTS = () => {
 
-  const [NFTcoin, setNFT] = useState([]);
-  const [loading, setLoading] = useState(false);
+  
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
-  // add or export a function to Format The currency 
-
-    // i will work on a static web page for the nft due the performance of the app and more cleaner or i will just try an open api 
+  
 }
 
 
