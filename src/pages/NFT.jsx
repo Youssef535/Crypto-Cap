781f3b54d1d6a77e57c52c290884089061ab3cf2
youssef535 <youssefsoltani938@gmail.com>
2022-10-17 17:58:20 +0200

    Update SEO Tags
    Still working on the SEO tags and redesign the web app and make it more clean especially on rendering

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -12,6 +12,7 @@ const NFT = () => {
   
   return (
     // So my problem now is to work on the SEO tags after that i need to find an NFT API AND fetch it to my app 
+    // i already have a plan for what i will add and probably i will use firebase for backend
     <>
     <h1 className='p-4'>Trending collections</h1>
     <Container>
