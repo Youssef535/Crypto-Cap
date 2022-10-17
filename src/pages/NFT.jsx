9610db6454bb6bb33ea3e85a44dff32acc55bef7
youssef535 <youssefsoltani938@gmail.com>
2022-10-17 10:49:45 +0200

    Update NFT.jsx
    I will build a static web page on the NFT section because the API won't work

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -1,7 +1,6 @@
-import {React,useState,useEffect} from 'react'
-import { Container, Form, Table, Pagination } from 'react-bootstrap'
+import {React,useState} from 'react'
+import { Container, Form, Pagination } from 'react-bootstrap'
 import axios from 'axios'
-//import { nft } from '../Config/api'
 
 
 const NFT = () => {
@@ -10,53 +9,15 @@ const NFT = () => {
   const [nfts, SetNFTs ] = useState([]);
   //const [search, setSearch] = useState("");
   //const [Page, setPage] = useState(1);
-  const  [Loading, SetLoading] = useState(false);   // describe the status of async requests
-
-  /*const getNFT = async () => {
-    SetLoading(true);
-    const data = await axios.get(nft);
-    SetNFTs(data);
-    SetLoading(false);
-  }; 
-  console.log(nfts);
-
-  useEffect(() => {
-    getNFT();
-  },[]); */
-
-  /*const handleChange = () => {
-    return NFTs.filter(NFTs => NFTs.name.toLowerCase().includes(search)) || [];
-  } */
-
-  // create the head of the table and then map throught the API content
-  // The API didn't work so i'm looking for to change the nft section to a static section image 
-  // flex grid and some js 
+  
   return (
+    // Build a static webpage API won't work
     <>
-    
     <h1 className='p-4'>Trending collections</h1>
     <Container>
-    <Table responsive  striped bordered hover variant="dark" className='tab'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Symbol</th>
-              <th>Contract Adress</th>
-            </tr>
-          </thead>
-          <tbdoy>
-            {nfts.map(nfts => (
-              <tr>
-                <td>{}</td>
-                <td>{}</td>
-                <td>{}</td>
-              </tr>
-            ))}
-      </tbdoy>
-    </Table>
+
     </Container>
     </>
-    
   )
 }
 
