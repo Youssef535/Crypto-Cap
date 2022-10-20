2fb8680699fa91903de11f7e2312d037682ff818
youssef535 <youssefsoltani938@gmail.com>
2022-10-20 11:11:58 +0200

    Change The style of the banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,15 +3,14 @@ import {Row,Col,Container,Form,Button} from 'react-bootstrap';
 import img from './logo.jpg';
 
 
-const Banner = () => {
+const Banner = () => {  // Working on styling more the app by adding Bootstrap class
   return (
     <div >
         <Container fluid="md">
           <Row>
-            <Col className='Col-1'>
+            <Col md={7} className='Col-1'>
             <h1 className='Heading1'>Jump start your crypto portfolio</h1>
             <h3 className='Heading2'>Coin Cap is the easiest place to buy and sell cryptocurrency. Sign up and get started today.</h3>
-        
           <Form.Control  className='input-1'
             aria-label="Example text with button addon"
             placeholder="Enter Your Email"
@@ -20,13 +19,10 @@ const Banner = () => {
         /> 
           <Button as="input" type="submit" value="Get Started" className='submit-btn' />
           </Col>
-        
-        <Col>
-        <img src={img} className='img-1' alt=""/>
+          <Col>
+          <img src={img} className='img-1' alt=""/>
         </Col>
-        
       </Row>
-      
       </Container>
     </div>
   );
