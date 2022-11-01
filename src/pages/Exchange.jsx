92b9dc41d1a19c77d5fae5148dfc6ef8097dfda4
youssef535 <youssefsoltani938@gmail.com>
2022-11-01 15:24:23 +0100

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import { Container, Form, Pagination, Table, Col, Row } from 'react-bootstrap'
+import { Axios } from 'axios'
 
 
-
+// 
 
 
 const Exchange = () => {
   return (
     <h1 className='p-4'>Exchange Market Rate </h1>
+    
   )
 }
 
