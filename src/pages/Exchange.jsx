448e4dc6022cd54b952c56d6cbc2e078710203a5
youssef535 <youssefsoltani938@gmail.com>
2022-11-09 20:59:52 +0100

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -8,6 +8,20 @@ const [loading, setLoading] = useState(false);
 const [page, setPage] = useState(1); 
 // const api = 'https://api.coincap.io/v2/exchanges';
 
+var config = {
+  method: 'get',
+  url: 'api.coincap.io/v2/exchanges',
+  headers: { }
+};
+
+axios.get(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
   return (
     <h1 className='p-4'>Exchange Market Rate </h1>
     
