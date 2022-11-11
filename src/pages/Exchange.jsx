8ab3010de16f23586c8fb1b416e877899fcea3a3
youssef535 <youssefsoltani938@gmail.com>
2022-11-11 21:13:03 +0100

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -9,13 +9,12 @@ const [page, setPage] = useState(1);
 // const api = 'https://api.coincap.io/v2/exchanges';
 
 const [news, setNews] = useState([]);
-  // Add a Pagination if you changed the api to crypto I REMOVED FOR THE PERFORMANCE
-
+  
   const newsAPI = async () =>{
-    // just change the api from top-headline to crypto the app is under development
+    
     // Please Visit newsapi.org
     let {data} = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5fca490bd4d54b23a7143e2fbab1dea9');
-    // eslint-disable-next-line no-lone-blocks
+    
     { /*console.log("news",data);*/ }
     setNews(data.articles);
   }
@@ -31,5 +30,5 @@ const [news, setNews] = useState([]);
     
   )
 }
-
+// Working on performance especially when react render the component {nesting, parent, child}
 export default Exchange
