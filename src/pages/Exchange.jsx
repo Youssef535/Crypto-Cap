3021cb533fa9f354e7a81f16d7ced9f48a891ffe
youssef535 <youssefsoltani938@gmail.com>
2022-11-11 16:22:53 +0100

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -8,19 +8,23 @@ const [loading, setLoading] = useState(false);
 const [page, setPage] = useState(1); 
 // const api = 'https://api.coincap.io/v2/exchanges';
 
-var config = {
-  method: 'get',
-  url: 'api.coincap.io/v2/exchanges',
-  headers: { }
-};
+const [news, setNews] = useState([]);
+  // Add a Pagination if you changed the api to crypto I REMOVED FOR THE PERFORMANCE
 
-axios.get(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+  const newsAPI = async () =>{
+    // just change the api from top-headline to crypto the app is under development
+    // Please Visit newsapi.org
+    let {data} = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5fca490bd4d54b23a7143e2fbab1dea9');
+    // eslint-disable-next-line no-lone-blocks
+    { /*console.log("news",data);*/ }
+    setNews(data.articles);
+  }
+
+  useEffect(() => {
+    //componentDidMount
+    newsAPI();
+  }, [])
+
 
   return (
     <h1 className='p-4'>Exchange Market Rate </h1>
