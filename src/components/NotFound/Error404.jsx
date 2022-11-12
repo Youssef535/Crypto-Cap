7f68053be84058ffed67e4242abc5b4c4fca47e1
youssef535 <youssefsoltani938@gmail.com>
2022-11-12 21:51:48 +0100

    Updating package.json

--- src/components/NotFound/Error404.jsx
+++ src/components/NotFound/Error404.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-
-
 function NotFound() {
   return (
-    
+    // just imort a png for the page
     <div className='d-flex align-items-center justify-content-center display-5'>
         4 0 4 
         
