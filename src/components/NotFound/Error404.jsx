c40ba80acab916746983b1116114c6752d4e2d38
youssef535 <youssefsoltani938@gmail.com>
2022-11-15 21:12:10 +0100

    Finsihed Error page

--- src/components/NotFound/Error404.jsx
+++ src/components/NotFound/Error404.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Button,Container,Row } from 'react-bootstrap'
+import {  useNavigate } from 'react-router-dom';
+// If You want import an img
 
 function NotFound() {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = '/'; 
+    navigate(path);
+  }
   return (
-    // just imort a png for the page
-    <div className='d-flex align-items-center justify-content-center display-5'>
-        4 0 4 
-        
+    <Container>
+      <Row>
+      <div className='pt-5'>
+      <h1 className='display-3 pt-5'>Page Not Found</h1>
+      <p className='fs-5 pt-4 '>Looks like you've followed a broken link or entered URL that doesn't exist on this site.</p>
+          <Button onClick={routeChange}>Back to our site</Button>
     </div>
+      </Row>
+    </Container>
   )
 }
 
