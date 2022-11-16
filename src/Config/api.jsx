05cc183fdb96cad2fcd91a96b4735e16a9b42dce
youssef535 <youssefsoltani938@gmail.com>
2022-11-16 20:09:48 +0100

    Working on API

--- src/Config/api.jsx
+++ src/Config/api.jsx
@@ -8,14 +8,6 @@ export const SingleCoin = (id) =>
 export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
 
+export const exchangesAPI = 'https://api.coingecko.com/api/v3/exchanges?per_page=10&page=1';
 
-/*import the trending coin API 
-
-export const trending =
-  'https://api.coingecko.com/api/v3/search/trending';
-
-// import NFT List BETA COINGECKO API (Still working on it )
-
-export const nft =  'https://api.coingecko.com/api/v3/nfts/list?order=h24_volume_native_asc&per_page=10&page=10'; // 404
-// try to work with coingecko api for the exchange
-*/
+// Working on Trending API maybe i will add the API.
--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -1,28 +1,33 @@
 import {React, useEffect, useState} from 'react'
 import { Container, Form, Pagination, Table, Col, Row } from 'react-bootstrap'
-import { axios } from 'axios'
+import  axios  from 'axios'
+import { exchangesAPI } from '../Config/api'
 
 const Exchange = () => {
 const [exchanges, SetExchanges] = useState([]);
 const [loading, setLoading] = useState(false);
 const [page, setPage] = useState(1); 
-const [news, setNews] = useState([]);  
+
   
-  const newsAPI = async () =>{
-    
-    // Please Visit newsapi.org
-    let {data} = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5fca490bd4d54b23a7143e2fbab1dea9');
-    
-    { /*console.log(data);*/ }
-    setNews(data.articles);
-  }
-  useEffect(() => {
-    //componentDidMount
-    newsAPI();
-  }, []);
+    // Please Visit coingecko api
+    const fetchExchanges = async () => {
+      setLoading(true);
+        const  data  = await axios.get(exchangesAPI);
+        SetExchanges(data);
+      setLoading(false);
+    };
+  
+    useEffect(() => {
+      fetchExchanges();
+    },[]);
+  
   return (
-    <h1 className='p-4'>Exchange Market Rate </h1>
-    
+    <Container>
+      <Row>
+      <h1 className='p-4'>Exchange Market Rate </h1>
+      
+      </Row>
+    </Container>
   )
 }
 
